57a2875314b91dfa0d3978527b36dbb83e71b0ec
Alexander Osin <alexandre.osin@gmail.com>
2017-02-02 18:03:49 +0200

    Created 3 new models.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Time, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
-engine = create_engine('sqlite:///app.db', echo=True)
 Base = declarative_base()
 
 class User(Base):
@@ -24,6 +24,8 @@ class Appointment(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    appointment_date = relationship('AppointmentDate', backref='appointments')
+    appointment_time = relationship('AppointmentTime', backref='appointments')
 
     def __init__(self, name):
         self.name = name
@@ -31,4 +33,38 @@ class Appointment(Base):
     def __repr__(self):
         return "<Appointment(id {}, name {})>".format(self.id, self.name)
 
-Base.metadata.create_all(engine)
+class AppointmentDate(Base):
+    __tablename__ = "appointment_dates"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    appointment_id = Column(Integer, ForeignKey('appointments.id'))
+
+    def __init__(self, date, appointment_id):
+        self.date = date
+        self.appointment_id = appointment_id
+
+    def __repr__(self):
+        return '<AppointmentDate(id {}, date {}, appointment {})>' \
+            .format(self.id, self.date, self.appointment_id)
+
+class AppointmentTime(Base):
+    __tablename__ = "appointment_times"
+
+    id = Column(Integer, primary_key=True)
+    start = Column(Time)
+    end = Column(Time)
+    appointment_id = Column(Integer, ForeignKey('appointments.id'))
+
+    def __init__(self, start, end, appointment_id):
+        self.start = start
+        self.end = end
+        self.appointment_id = appointment_id
+    
+    def __repr__(self):
+        return '<AppointmentTime(id {}, start {}, end {}, appointment {})>' \
+            .format(self.id, self.start, self.end, self.appointment_id)
+
+if __name__=='__main__':
+    engine = create_engine('sqlite:///app.db', echo=True)
+    Base.metadata.create_all(engine)
--- testdata.py
+++ testdata.py
@@ -1,6 +1,7 @@
 from app import engine
 from sqlalchemy.orm import sessionmaker
-from app.models import User, Appointment
+from app.models import User, Appointment, AppointmentDate, AppointmentTime
+import datetime
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -9,7 +10,12 @@ session.add_all([
     User('admin', 'password'),
     User('1', '1'),
     Appointment('First'),
-    Appointment('Second')
+    Appointment('Second'),
+    AppointmentDate(date=datetime.date(2017, 3, 1), appointment_id=1),
+    AppointmentDate(date=datetime.date(2017, 3, 3), appointment_id=1),
+    AppointmentDate(date=datetime.date(2017, 3, 20), appointment_id=2),
+    AppointmentTime(start=datetime.time(10), end=datetime.time(11), appointment_id=1),
+    AppointmentTime(start=datetime.time(20), end=datetime.time(21), appointment_id=2)
 ])
 
 session.commit()
