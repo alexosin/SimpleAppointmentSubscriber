a5cbb39307c197432d136c6fd51fe19c9461fe1d
Alexander Osin <alexandre.osin@gmail.com>
2017-02-02 17:44:29 +0200

    Added login and appointment views.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 engine = create_engine('sqlite:///app.db', echo=True)
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,7 +1,37 @@
-from app import app
-from flask import render_template
+from app import app, engine
+from flask import render_template, redirect, request, session, flash, url_for
+from sqlalchemy.orm import sessionmaker
+from .models import User, Appointment
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/appointments')
+def main():
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(Appointment).all()
+    return render_template('appointments.html', appoints=query)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        Session = sessionmaker(bind=engine)
+        s = Session()
+        query = s.query(User).filter(User.username.in_ \
+            ([username]), User.password.in_([password])).first()
+
+        if query:
+            session['logged_in'] = True
+            session['user'] = username
+        else:
+            flash('Invalid login or password.')
+        return redirect(url_for('main'))
+    else:
+        return render_template('login.html')
+
